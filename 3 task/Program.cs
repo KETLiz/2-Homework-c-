dc5b7472384d4d948b83ecb98f9ec23261029d2e
liza <varlamova_e.t@mail.ru>
2022-08-24 10:47:39 +0300

    8 c

--- 3 task/Program.cs
+++ 3 task/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void Holiday()
+{
+    if (x == 6 || x == 7)
+    {
+        Console.WriteLine("Выходной! Ура!");
+    }
+    else
+    {
+        Console.WriteLine("Иди на работу, лентяй!");
+    }
+    
+}
+Holiday();
