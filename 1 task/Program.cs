c9474afe7d408f5ee3452de08c541cb5f6b16350
liza <varlamova_e.t@mail.ru>
2022-08-24 10:11:12 +0300

    3 commit

--- 1 task/Program.cs
+++ 1 task/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void PrintNumber ()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    int a = x % 100;
+    int b = a / 10;
+    Console.WriteLine($"Второе число из трехзначного числа равно {b}");
+}
+PrintNumber();
